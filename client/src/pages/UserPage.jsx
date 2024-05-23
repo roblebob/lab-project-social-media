1e277ed1e30ffc863fa58573727c0003b5644f36
roblebob <robert.koppisch@gmail.com>
2024-05-23 16:34:43 +0200

    add UserPost and Actions

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,11 +1,14 @@
 import UserHeader from "../components/UserHeader";
+import UserPost from "../components/UserPost";
 
 const UserPage = () => {
-  return (
-    <>
-      <UserHeader />
-    </>
-  );
+  return <>
+    <UserHeader />
+    <UserPost likes={1200} replies={401} postImage={"/post1.png"} postTitle={"Let'S talk"}/>
+    <UserPost likes={450} replies={41} postImage={"/post2.png"} postTitle={"Let'S talk"}/>
+    <UserPost likes={200} replies={50} postImage={"/post3.png"} postTitle={"Let'S talk"}/>
+    <UserPost likes={100} replies={101}  postTitle={"Let'S talk"}/>
+  </>;
 };
 
 export default UserPage;
