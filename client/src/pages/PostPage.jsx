fc893bb940cf36e9bc7ff180fcbe2cbb0e9e0a7a
roblebob <robert.koppisch@gmail.com>
2024-05-23 17:47:34 +0200

    start implementing the PostPage

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,7 +1,39 @@
+import { Flex, Avatar, Text, Image, Box } from "@chakra-ui/react";
+import { BsThreeDots } from "react-icons/bs";
+
 const PostPage = () => {
   return (
-    <div>PostPage</div>
-  )
-}
+    <>
+      <Flex>
+        <Flex w={"full"} alignItems={"center"} gap={3}>
+          <Avatar name="Mark Zuckerberg" src="/zuck-avatar.png" size={"md"} />
+          <Flex>
+            <Text fontSize={"sm"} fontWeight={"bold"}>
+              markzuckerberg
+            </Text>
+            <Image src="/verified.png" w={4} h={4} ml={4} />
+          </Flex>
+        </Flex>
 
-export default PostPage
+        <Flex gap={4} alignItems={"center"}>
+          <Text fontSize={"sm"} color={"gray.light"}>
+            1d
+          </Text>
+          <BsThreeDots />
+        </Flex>
+      </Flex>
+
+      <Text my={3}>Let's talk about</Text>
+      <Box
+        borderRadius={6}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"gray.light"}
+      >
+        <Image src={"/post1.png"} w={"full"} />
+      </Box>
+    </>
+  );
+};
+
+export default PostPage;
