ddffce6755d1ea6454f68cd2c746862686a54551
roblebob <robert.koppisch@gmail.com>
2024-05-23 11:44:33 +0200

    include Navbar, for controlling different themes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
 import { Container } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Container maxWidth="640px">
+      <Navbar />
       <Routes>
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/posts/:postId" element={<PostPage />} />
