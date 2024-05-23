b86d592884a2a4e0663b24ee76dc57337a3eeebd
roblebob <robert.koppisch@gmail.com>
2024-05-23 11:18:33 +0200

    in App.jsx, wrap the entire app within a 640px container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
+import { Container } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/:username" element={<UserPage />} />
-      <Route path="/:username/posts/:postId" element={<PostPage />} />
-    </Routes>
+    <Container maxWidth="640px">
+      <Routes>
+        <Route path="/:username" element={<UserPage />} />
+        <Route path="/:username/posts/:postId" element={<PostPage />} />
+      </Routes>
+    </Container>
   );
 }
 
