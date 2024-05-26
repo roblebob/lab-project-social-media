102e702e2cf34dd28991d1316c1acc2a10dbbb76
roblebob <robert.koppisch@gmail.com>
2024-05-27 00:14:03 +0200

    @5:29:54 update Spinner for UserPage

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -3,14 +3,15 @@ import UserHeader from "../components/UserHeader";
 import UserPost from "../components/UserPost";
 import { useParams } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
+import { Flex, Spinner } from "@chakra-ui/react";
 
 const UserPage = () => {
   const [user, setUser] = useState(null);
   const { username } = useParams();
   const showToast = useShowToast();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     const getUser = async () => {
       try {
         const res = await fetch(`/api/users/profile/${username}`);
@@ -22,21 +23,28 @@ const UserPage = () => {
         }
 
         setUser(data);
-
       } catch (error) {
         showToast("Error", error, "error");
+      } finally {
+        setLoading(false);
       }
     };
 
     getUser();
-
   }, [username, showToast]);
 
-  if (!user) return null;
-  
+  if (!user && loading)
+    return (
+      <Flex justify={"center"}>
+        <Spinner size={"xl"} />
+      </Flex>
+    );
+
+  if (!user && !loading) return <h1>User not found</h1>;
+
   return (
     <>
-      <UserHeader user={user}/>
+      <UserHeader user={user} />
       <UserPost
         likes={1200}
         replies={401}
